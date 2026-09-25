c7710646c306cb86decf8da66e81a9c191db1064
husanboy20090211-dot <husanboy20090211@gmail.com>
2026-09-25 07:32:55 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function sendForm(e){
+  e.preventDefault();
+  document.getElementById("formMsg").textContent =
+    "Arizangiz qabul qilindi. Aloqa ma'lumotlarini ulash uchun formani Telegram, WhatsApp yoki CRM bilan bog'lash mumkin.";
+}
